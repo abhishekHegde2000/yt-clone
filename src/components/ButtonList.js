9d1cafba4dbfd6dfd23b03ea33dbf3f59d9d8e86
abhishek Hegde <ahegde2021@gmail.com>
2023-10-06 17:17:26 +0530

    Commit - 3
    used fetch function to successfully extract thed data,
    
    i was getting only 5 vids , so used max50 parameter in api .
    
    looped over videos from fetched data and passed props to videocards to get the ui correctly

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,9 +1,63 @@
-import React from 'react'
+// import React from "react";
+// import Button from "./Button";
+
+// const list = ["All", "Live", "Gaming"];
+
+// const ButtonList = () => {
+
+//   // return (
+
+//     // <div className="flex">
+//     //   <Button name="All" />
+//     //   <Button name="Gaming" />
+//     //   <Button name="Songs" />
+//     //   <Button name="Live" />
+//     //   <Button name="Soccer" />
+//     //   <Button name="Cricket" />
+//     //   <Button name="Cooking" />
+//     //   <Button name="Cricket" />
+//     //   <Button name="Valentines" />
+//     //   <Button name="Cricket" />
+//     //   <Button name="Cooking" />
+//     //   <Button name="Cricket" />
+//     // </div>
+
+//   // );
+// };
+
+// export default ButtonList;
+
+
+import React from "react";
+import Button from "./Button";
+
+const list = 
+[
+  "All",
+  "Live",
+  "Gaming",
+  "Songs",
+  "Soccer",
+  "Cricket",
+  "Cooking",
+  "Valentines",
+  "All",
+  "Live",
+  "Gaming",
+  "Songs",
+
+  // add more items here
+];
 
 const ButtonList = () => {
   return (
-    <div>ButtonList</div>
-  )
-}
+    <div className="flex">
+      {list.map((name, index) => (
+        <Button key={index} name={name} />
+      ))}
+    </div>
+  );
+};
 
-export default ButtonList
+
+export default ButtonList;
--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,5 +1,7 @@
-const GOOGLE_API_KEY = "AIzaSyCfXXzU7wimn1HnC_vecAn1ss7SsODDyUs";
+const GOOGLE_API_KEY = "AIzaSyA0diAmgUiVwmrm_-Xrm93PJTVCLl2ilTk";
 
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
   GOOGLE_API_KEY;
+
+
