3d75a362c88a3cc9a6d35f2c45232d2051320236
abhishek Hegde <ahegde2021@gmail.com>
2023-10-06 14:13:53 +0530

    Commit - 1
    Created basic ui
    installed redux toolkit and react redux
    create a store with createStore api
    created slice with createSlice
    Gave the slice back to  Store inside reducers.
    
    Provide the Store to app through provider store = {store}

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from './components/Head';
+import Body from './components/Body';
+import { Provider } from 'react-redux';
+import store from './utils/store';
+
+
+function App(){
+  return(
+    <Provider store={store}>
+      <Head />
+      <Body />
+    </Provider>
+  )
+}
+
+export default App; 
